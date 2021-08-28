db77d4c024b6a62468941beb173fd5e345c8c4ef
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-28 14:12:26 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -7,3 +7,12 @@ print("{0}+{1}={2}".format(a,b,c))
 #b=int(input("Enter the second number"))
 #c=a+b
 #print(a,"+",b,"=",c)
+#2nd method
+#a=int(input("Enter the first number"))
+#b=int(input("Enter the second number"))
+#c=a+b
+#print("%d + %d = %d"%(a,b,c))
+#3rd method
+#a=int(input("Enter the first number"))
+#b=int(input("Enter the second number"))
+#print("The sum is :",(a+b))
