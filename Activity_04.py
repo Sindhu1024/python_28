62a39e922cab7d9437df444510f591fbf51fb81d
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-28 14:05:52 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -2,3 +2,8 @@ a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
 c=a+b
 print("{0}+{1}={2}".format(a,b,c))
+#other methods
+#a=int(input("Enter the first number"))
+#b=int(input("Enter the second number"))
+#c=a+b
+#print(a,"+",b,"=",c)
