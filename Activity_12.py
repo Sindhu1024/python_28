ac650a1ba23cec416af07858ec7a079f1f93c593
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-29 14:40:47 +0530

    Update Activity_12.py

---
+++ Activity_12.py
@@ -0,0 +1,17 @@
+def input1():
+    a=int(input("Enter first number"))
+    b=int(input("Enter second number"))
+    c=int(input("Enter third number"))
+    return a,b,c
+def compare(a,b,c):
+    if(a>b and a>c):
+        print("{0} is the greatest number among {0},{1} and {2}".format(a,b,c))
+    elif(b>a and b>c):
+        print("{0} is the greatest number among {1},{0} and {2}".format(b,a,c))
+    else:
+        print("{0} is the greatest number among {1},{2} and {0}".format(c,a,b))
+
+def main():
+    x,y,z=input1()
+    compare(x,y,z)
+main()
