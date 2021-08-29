d98f3a9fd161d88141e36b5a94dc94bcf10299a4
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-29 10:30:04 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -1,3 +1,4 @@
+import math
 l=float(input("Enter the lenght of the tromboloid"))
 b=float(input("Enter the breadth of the tromboloid"))
 h=float(input("Enter the height of the tromboloid"))
@@ -7,7 +8,8 @@ volume=a/(k**.5)
 v=round(volume,3)
 print("The volume is:",v)
 r2=volume*.75
-r1=r2/3.14
+c=math.pi
+r1=r2/c
 r=(r1)**(1/3)
 radius=round(r,3)
 print("The radius of the sphere is:",radius)
