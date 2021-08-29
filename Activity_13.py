36bdba04176e0c5f9f3889b1807cdbc3cb404442
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-29 17:21:11 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -3,3 +3,9 @@ e=int(input("Enter the stop number"))
 for i in range(s,e+1):
     if i%2 !=0:
         print(i)
+        
+        
+#s=int(input("Enter the start number"))
+#e=int(input("Enter the stop number"))
+#a=list(range(s,e+1,2))
+#print(a)
