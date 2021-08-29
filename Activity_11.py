f94a40be64d0704a8f486b38dc7cc022b722ca65
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-29 13:39:10 +0530

    Update Activity_11.py

---
+++ Activity_11.py
@@ -0,0 +1,17 @@
+def input_number():
+	num1 = int(input("Enter the first number: "))
+	num2 = int(input("Enter the second number: "))
+	return num1, num2
+
+def add(a, b):
+	return a+b
+
+def display(a, b, c):
+	print(f"{a} + {b} = {c}")
+
+
+def main():
+	num1, num2 = input_number()
+	total= add(num1, num2)
+	display(num1, num2, total)
+main()
