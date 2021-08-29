a83efdf3a3b685f12262d181c7403b215e15a80e
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-29 15:44:07 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -15,3 +15,15 @@ def main():
     x,y,z=input1()
     compare(x,y,z)
 main()
+
+#def input1():
+ #   a=int(input("Enter first number"))
+ #  b=int(input("Enter second number"))
+ # c=int(input("Enter third number"))
+ #return a,b,c
+#def great_terenery(a,b,c):
+   #max=print(a) if a>b and a>c else print(b) if b>c else print(c)
+#def main():
+    #x,y,z=input1()
+    #great_terenery(x,y,z)
+#main()
