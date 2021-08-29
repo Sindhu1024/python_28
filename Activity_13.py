eb2ede8a74b11f530e953d0d1afa22e1f784209e
Sindhu1024 <89655026+Sindhu1024@users.noreply.github.com>
2021-08-29 17:52:36 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -1,8 +1,21 @@
-s=int(input("Enter the start number"))
-e=int(input("Enter the stop number"))
-for i in range(s,e+1):
-    if i%2 !=0:
-        print(i)
+def inp(): 
+    s=int(input("Enter the start number"))
+    e=int(input("Enter the stop number"))
+    return s,e
+def odd(s,e):
+    for i in range(s,e+1):
+        if i%2 !=0:
+            print(i)
+def main():
+    p,q=inp()
+    odd(p,q)
+main()
+
+#s=int(input("Enter the start number"))
+#e=int(input("Enter the stop number"))
+#for i in range(s,e+1):
+ #   if i%2 !=0:
+ #       print(i)
         
         
 #s=int(input("Enter the start number"))
